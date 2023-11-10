79757aad6e97db1a5d9e43df2c82229f7ce930b3
samvelMelikyan <122483834+samvelMelikyan@users.noreply.github.com>
2023-11-10 16:59:39 +0400

    10.11.23 Filter Test completed.

--- src/main/java/com/swaglabs/pages/Home.java
+++ src/main/java/com/swaglabs/pages/Home.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8ba7ed63cda14f3789b23225ddec096ead9fd33acdcbdc7ecc8040561966cc05
-size 3587
+oid sha256:c2d2531e96f58c08bd82ce2df6cbeb73eb312cfa1061d1baa33c5508138c7219
+size 5650
--- src/test/java/swagLabs/FilterTest.java
+++ src/test/java/swagLabs/FilterTest.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d03c8a0d150ee10799714a8ec15d57c1e9c80a9dfc4f19a1e28f8f0614b6db15
-size 377
+oid sha256:9446cd41fe272ebbd4b8f74bd4a2997951166501eac5a3c5a72d9f8682cab905
+size 956
