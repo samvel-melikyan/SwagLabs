07fd2e8849d67af7ef2f80b79279098cbb977363
samvelMelikyan <122483834+samvelMelikyan@users.noreply.github.com>
2023-11-11 15:02:05 +0400

    10.11.23 Filter Test completed.

--- src/test/java/swagLabs/AllItemTest.java
+++ src/test/java/swagLabs/AllItemTest.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ce7cbc6ad9714d3c109c2baeab6d2060fbf8bc2ec2cd3a6746b14981e65a97f0
-size 126
+oid sha256:bedc8cbd5d9dbf3581180ac831afdb7b0430c00d40b2354e04ccab5f63d0f2ee
+size 117
