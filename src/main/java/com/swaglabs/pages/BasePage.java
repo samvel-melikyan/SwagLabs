ec2cb44b1d3088ce64677b057fb22aa761e50f3a
samvelMelikyan <samvel.melikyan.eng@gmail.com>
2023-11-12 16:43:50 +0400

    All Items Test 12.11.2023

--- src/main/java/com/swaglabs/pages/BasePage.java
+++ src/main/java/com/swaglabs/pages/BasePage.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b04afd1bff13a10976dabd8cce1261242dc3cac245881df2096c6047c9851a70
-size 3170
+oid sha256:b8fb764d03abca6803dc8d482ba9beff4e884ac3ee6b119ca726599d9368ab60
+size 3314
