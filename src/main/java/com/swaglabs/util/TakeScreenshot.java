baed9de3ebcd5abca5f3289dcb472e26104f1cc9
samvelMelikyan <samvel.melikyan.eng@gmail.com>
2024-01-01 23:59:36 +0400

    01.01.2024

--- src/main/java/com/swaglabs/util/TakeScreenshot.java
+++ src/main/java/com/swaglabs/util/TakeScreenshot.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b61e4b81c9c082fc372be8ce61e1955858bb92969dd26858fc207bbb997e78dc
-size 2347
+oid sha256:24d123f8d4db38518b7f450960a89bb8506b13fe9f5ad74edb428f40dd02da65
+size 2212
--- src/test/java/swagLabs/HomeTest.java
+++ src/test/java/swagLabs/HomeTest.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:786d8a8615bd764f24ffb25bfb4f22f39712cd33a41e666376544502b826eb9a
-size 6529
+oid sha256:dd2dd4711cf5a2cc338d25303c4f2aba7bb9f0d523839091a02392fe8b8e37b1
+size 8286
